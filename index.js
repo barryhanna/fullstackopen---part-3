8037e12480c55889fdc6d7c7702486ab6bbdc1cd
Barry Hanna <barryahanna@gmail.com>
2023-06-23 13:24:48 +0100

    Refactored db code to model

--- index.js
+++ index.js
@@ -2,27 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const mongoose = require('mongoose');
-
-const url = `mongodb+srv://barryahanna:${process.env.MONGO_DB_PASSWORD}@notes.pxqazfi.mongodb.net/noteApp`;
-
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-	content: String,
-	important: Boolean,
-});
-
-noteSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
-});
-
-const Note = mongoose.model('Note', noteSchema);
+const Note = require('./models/note');
 
 app.use(express.static('build'));
 app.use(cors());
@@ -57,14 +37,10 @@ app.get('/api/notes', (req, res) => {
 	});
 });
 
-app.get('/api/notes/:id', (req, res) => {
-	const id = Number(req.params.id);
-	const note = notes.find((note) => note.id === id);
-	if (note) {
-		res.json(note);
-	} else {
-		res.status(404).end();
-	}
+app.get('/api/notes/:id', (request, response) => {
+	Note.findById(request.params.id).then((note) => {
+		response.json(note);
+	});
 });
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -79,24 +55,24 @@ const generateId = () => {
 	return maxId + 1;
 };
 
-app.post('/api/notes', (req, res) => {
-	const body = req.body;
+app.post('/api/notes', (request, response) => {
+	const body = request.body;
 
-	if (!body.content) {
-		return res.status(400).json({ error: 'content missing' });
+	if (body.content === undefined) {
+		return response.status(400).json({ error: 'content missing' });
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
-		id: generateId(),
-	};
+	});
 
-	notes = notes.concat(note);
-	res.json(note);
+	note.save().then((savedNote) => {
+		response.json(savedNote);
+	});
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
